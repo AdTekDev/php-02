a742ec9bd7d015fabaaeacf40f643374f3ad004c
AdTekDev <nguyenngtu777@gmail.com>
2020-07-01 11:26:14 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php 
 
     $active='Home';
-    include("includes/header.php");
+	
+	echo "hello !!!";
 
 ?>
 
@@ -136,17 +137,13 @@
        
        <div class="row"><!-- row Begin -->
 
-       <?php 
-            getpro();
-       ?>
+
            
        </div><!-- row Finish -->
        
    </div><!-- container Finish -->
 
-   <?php
-        include("includes/footer.php");
-   ?>
+
     
     <script src="js/jquery-331.min.js"></script>
     <script src="js/bootstrap-337.min.js"></script>
